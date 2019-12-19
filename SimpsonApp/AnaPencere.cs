595a4c8ad0e1e39fb3dc15e6cdf99e73195eca87
Kemalettin Aktaş <kemalettin.aktas@gazi.edu.tr>
2019-12-19 10:41:20 +0300

    Proje dosyası ekle.

---
+++ SimpsonApp/AnaPencere.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SimpsonApp
+{
+    class AnaPencere:Form
+    {
+        Image resim;
+        public AnaPencere(int genislik, int yukseklik)
+        {
+            
+            this.Width = genislik;
+            this.Height = yukseklik;
+            resim = Image.FromFile("android.jpg");
+            Paint += AnaPencere_Paint;
+
+
+        }
+
+        private void AnaPencere_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(resim, 0, 0);
+        }
+    }
+}
